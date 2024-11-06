31f40f13b77ec27c8dad50c69bbafc85d6ada6c1
Charles Goodsir <charlesguni2024@gmail.com>
2024-11-06 18:11:31 +1300

    Movie model, pages and server added

---
+++ client/pages/Movie.tsx
@@ -0,0 +1,22 @@
+import request from 'superagent'
+import { useQuery } from '@tanstack/react-query'
+import { Movie as MovieModel } from '../models/Movies'
+
+function useMovie(movieTitle: string) {
+  return useQuery<MovieModel, Error>({
+    queryKey: ['movies', movieTitle],
+    queryFn: async () => {
+      const apiUrl = `/api/v1/movie?title=${movieTitle}`
+
+      const res = await request.get(apiUrl)
+
+      if (res.body.error) {
+        throw new Error(res.body.error)
+      }
+
+      return res.body as MovieModel
+    },
+  })
+}
+
+export default useMovie
--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,38 @@
 import * as Path from 'node:path'
 import express from 'express'
 import cors, { CorsOptions } from 'cors'
+import request from 'superagent'
+import dotenv from 'dotenv'
+dotenv.config()
 
+//I wrote down a console log so that when you run server it should display your keyu, if it doesn't it'll be undefined
 const server = express()
+console.log('OMDB API Key:', process.env.OMDB_API_KEY)
+server.get('/api/v1/movie', async (req, res) => {
+  const movieTitle = req.query.title as string
+  if (!movieTitle) {
+    return res.status(400).json({ error: 'Movie title is required' })
+  }
+  //Make sure you have an API key set up in .env
+  const apiKey = process.env.OMDB_API_KEY
+  if (!apiKey) {
+    return res.status(500).json({ error: 'OMDB API key is missing' })
+  }
 
-server.get('/api/v1/greeting', (req, res) => {
-  const greetings = ['hola', 'hi', 'hello', 'howdy']
-  const index = Math.floor(Math.random() * greetings.length)
-  console.log(index)
-  res.json({ greeting: greetings[index] })
+  const url = `http://www.omdbapi.com/?t=${movieTitle}&apikey=${apiKey}`
+
+  try {
+    const movieRes = await request.get(url)
+
+    if (movieRes.body.Response === 'False') {
+      return res.status(404).json({ error: movieRes.body.Error })
+    }
+
+    res.json(movieRes.body)
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: 'Failed to fetch movie details' })
+  }
 })
 
 server.use(express.json())
