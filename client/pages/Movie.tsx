3fcd4c90325e8221708d17e00680cb3baf1d6b98
Charles Goodsir <charlesguni2024@gmail.com>
2024-11-06 18:17:20 +1300

    Oops

--- client/pages/Movie.tsx
+++ client/pages/Movie.tsx
@@ -1,22 +1,22 @@
-import request from 'superagent'
-import { useQuery } from '@tanstack/react-query'
-import { Movie as MovieModel } from '../models/Movies'
+// import request from 'superagent'
+// import { useQuery } from '@tanstack/react-query'
+// import { Movie as MovieModel } from '../models/Movies'
 
-function useMovie(movieTitle: string) {
-  return useQuery<MovieModel, Error>({
-    queryKey: ['movies', movieTitle],
-    queryFn: async () => {
-      const apiUrl = `/api/v1/movie?title=${movieTitle}`
+// function useMovie(movieTitle: string) {
+//   return useQuery<MovieModel, Error>({
+//     queryKey: ['movies', movieTitle],
+//     queryFn: async () => {
+//       const apiUrl = `/api/v1/movie?title=${movieTitle}`
 
-      const res = await request.get(apiUrl)
+//       const res = await request.get(apiUrl)
 
-      if (res.body.error) {
-        throw new Error(res.body.error)
-      }
+//       if (res.body.error) {
+//         throw new Error(res.body.error)
+//       }
 
-      return res.body as MovieModel
-    },
-  })
-}
+//       return res.body as MovieModel
+//     },
+//   })
+// }
 
-export default useMovie
+// export default useMovie
