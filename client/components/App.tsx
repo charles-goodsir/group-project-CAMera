ca7eea99de4f637c7ac0c5f3ac05b5727720ef6f
Mel Hew <meleniehew@gmail.com>
2024-11-08 08:45:51 +1300

    handle change and handle submit

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,55 +1,41 @@
-import { useState } from 'react'
-import { useGetByYear, useMovie } from '../apiClient.ts'
+import { useState, FormEvent, ChangeEvent  } from 'react'
+import { useMovie } from '../apiClient.ts'
 import { useQuery } from '@tanstack/react-query'
 
-// const App = () => {
-//   const [count, setCount] = useState(0)
-
-//   const {
-//     data: greeting,
-//     isError,
-//     isPending,
-//   } = useQuery({ queryKey: ['Movie', count], queryFn: useMovie })
-
-//   if (isPending) return <p>Loading...</p>
-
-//   return (
-//     <>
-//       {useMovie}
-//       {count}
-//       <h1>Movie by year</h1>
-//       {isError && (
-//         <p style={{ color: 'red' }}>
-//           There was an error retrieving the greeting.
-//         </p>
-//       )}
-//       <button onClick={() => setCount(count + 1)}>Click</button>
-//     </>
-//   )
-// }
 
 const App = () => {
   const [year, setYear] = useState('')
-
+  
+  
   const {
     data: Movie,
     isError,
     isPending,
   } = useQuery({ queryKey: ['Movie', year], queryFn: useMovie })
 
+    async function handleSubmit(evt: FormEvent<HTMLFormElement>) {
+    evt.preventDefault()}
+
+    function handleChange(evt: ChangeEvent<HTMLInputElement>) {
+      setYear(evt.currentTarget.value);
+    }
+    
+
   if (isPending) return <p>Grabbing your movie!</p>
 
   if (isError) return <p>Cannot find your movie soz</p>
 
+  
+
   return (
-    <>
+    <form onSubmit={handleSubmit} className="form">
       <input
       type="number" 
       value={year} 
       placeholder="Enter a year"
-      onChange={(e) => setYear(e.target.value)} 
+      onChange={handleChange} 
       />
-    </>
+    </form>
   )
 }
 
