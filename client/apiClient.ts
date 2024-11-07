ece218617fe163633db91ffb35c07baed9b8cb8f
Alysha Dosdos <alyshadosdos@gmail.com>
2024-11-07 14:55:00 +1300

    added api functions

--- client/apiClient.ts
+++ client/apiClient.ts
@@ -1,6 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
 import request from 'superagent'
+import { Movie } from './models/Movies'
 
-export async function getGreeting(): Promise<string> {
-  const res = await request.get('/api/v1/greeting')
-  return res.body.greeting
+// export async function getMovie(): Promise<string> {
+//   const res = await request.get('/api/v1/movie')
+//   return res.body.title
+// }
+
+export function useMovie() {
+  return useQuery({
+    queryKey: ['movies'],
+    queryFn: async () => {
+      const res = await request.get('/api/v1/movie')
+      return res.body.movie
+    },
+  })
+}
+
+export function useGetByYear(year: number) {
+  return useQuery({
+    queryKey: ['movies', year],
+    queryFn: async () => {
+      const res = await request.get(`/api/v1/movie/${year}`)
+      return res.body as Movie
+    },
+  })
 }
--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,28 +1,49 @@
 import { useState } from 'react'
-import { getGreeting } from '../apiClient.ts'
+import { useGetByYear, useMovie } from '../apiClient.ts'
 import { useQuery } from '@tanstack/react-query'
 
+// const App = () => {
+//   const [count, setCount] = useState(0)
+
+//   const {
+//     data: greeting,
+//     isError,
+//     isPending,
+//   } = useQuery({ queryKey: ['Movie', count], queryFn: useMovie })
+
+//   if (isPending) return <p>Loading...</p>
+
+//   return (
+//     <>
+//       {useMovie}
+//       {count}
+//       <h1>Movie by year</h1>
+//       {isError && (
+//         <p style={{ color: 'red' }}>
+//           There was an error retrieving the greeting.
+//         </p>
+//       )}
+//       <button onClick={() => setCount(count + 1)}>Click</button>
+//     </>
+//   )
+// }
+
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [year, setYear] = useState('')
 
   const {
-    data: greeting,
+    data: Movie,
     isError,
     isPending,
-  } = useQuery({ queryKey: ['greeting', count], queryFn: getGreeting })
+  } = useQuery({ queryKey: ['Movie', year], queryFn: useMovie })
 
-  if (isPending) return <p>Loading...</p>
+  if (isPending) return <p>Grabbing your movie!</p>
+
+  if (isError) return <p>Cannot find your movie soz</p>
 
   return (
     <>
-      {count}
-      <h1>{greeting}</h1>
-      {isError && (
-        <p style={{ color: 'red' }}>
-          There was an error retrieving the greeting.
-        </p>
-      )}
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <input type="number" value={year} placeholder="Enter a year"></input>
     </>
   )
 }
